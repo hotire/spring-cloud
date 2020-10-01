c6a75e4b99c99d25b322953a45a9da01870a061e
hotire <gngh0101@gmail.com>
2020-10-01 13:48:42 +0900

    Add LoadbalancerService

---
+++ loadbalancer/src/main/java/com/github/hotire/loadbalancer/LoadbalancerService.java
@@ -0,0 +1,27 @@
+package com.github.hotire.loadbalancer;
+
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class LoadbalancerService {
+    private final WebClient webClient;
+
+    public LoadbalancerService(final WebClient.Builder webClientBuilder, final ReactorLoadBalancerExchangeFilterFunction lbFunction) {
+        this.webClient = webClientBuilder.filter(lbFunction).build();
+    }
+
+    public Mono<String> hello() {
+        return webClient.get()
+                        .uri("http://web-service/hello")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .log();
+    }
+
+
+
+}
