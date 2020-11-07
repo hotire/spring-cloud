6635fc6f85ac0fee9b227146afddf768cc0b5aa2
hotire <gngh0101@gmail.com>
2020-11-08 08:48:38 +0900

    Add EnableConfigServer annoation

--- config/src/main/java/com/github/hotire/config/Application.java
+++ config/src/main/java/com/github/hotire/config/Application.java
@@ -2,7 +2,9 @@ package com.github.hotire.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class Application {
     public static void main(String ... args) {
