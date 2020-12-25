05a7e62781c3ff135237ca14a56cac502a83ae0e
hotire <gngh0101@gmail.com>
2020-12-25 12:27:42 +0900

    Add KafkaProducer

---
+++ web-service/src/main/java/com/github/hotire/webservice/kafka/KafkaProducer.java
@@ -0,0 +1,19 @@
+package com.github.hotire.webservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String payload) {
+        log.info("sending payload='{}' to topic='{}'", payload, topic);
+        kafkaTemplate.send(topic, payload);
+    }
+}
