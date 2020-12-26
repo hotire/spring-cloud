4c715ae798b189cabc791c808a64bd562a8f011d
hotire <gngh0101@gmail.com>
2020-12-26 16:59:44 +0900

    Add KafkaConsumer KafkaProducer dependency to EmbeddedKafkaIntegrationTest

--- web-service/src/test/java/com/github/hotire/webservice/EmbeddedKafkaIntegrationTest.java
+++ web-service/src/test/java/com/github/hotire/webservice/EmbeddedKafkaIntegrationTest.java
@@ -4,8 +4,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.github.hotire.webservice.kafka.KafkaConsumer;
+import com.github.hotire.webservice.kafka.KafkaProducer;
+
+import lombok.RequiredArgsConstructor;
+
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@RequiredArgsConstructor
 public class EmbeddedKafkaIntegrationTest {
+    private final KafkaConsumer consumer;
+    private final KafkaProducer producer;
 }
