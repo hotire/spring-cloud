45d9fcbab4da4c4e8544d0b77fe5c8bbb99e5736
hotire <gngh0101@gmail.com>
2020-12-26 16:57:32 +0900

    Add kafka-test dependency

---
+++ web-service/src/test/java/com/github/hotire/webservice/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.github.hotire.webservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+public class EmbeddedKafkaIntegrationTest {
+}
