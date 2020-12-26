57b860cf29581d8446df630b1268b0bc61e55077
hotire <gngh0101@gmail.com>
2020-12-26 17:02:17 +0900

    Add CountDownLatch to KafkaConsumer

--- web-service/src/main/java/com/github/hotire/webservice/kafka/KafkaConsumer.java
+++ web-service/src/main/java/com/github/hotire/webservice/kafka/KafkaConsumer.java
@@ -1,15 +1,21 @@
 package com.github.hotire.webservice.kafka;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class KafkaConsumer {
+
+    @Getter
+    private final CountDownLatch latch = new CountDownLatch(1);
 
     @KafkaListener(topics = "${test.topic}")
     public void receive(ConsumerRecord<?, ?> consumerRecord, Acknowledgment ack) {
