7ad9325355f70bacfa83cc9dffbbb2dc747f8aae
hotire <gngh0101@gmail.com>
2021-10-17 13:47:04 +0900

    Learn FeignException

--- web-service/src/main/java/com/github/hotire/webservice/feign/exception/CustomFeignException.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/exception/CustomFeignException.java
@@ -3,6 +3,11 @@ package com.github.hotire.webservice.feign.exception;
 import feign.FeignException;
 import feign.Request;
 
+/**
+ * request is nullable
+ * responseBody is nullable
+ * contentUTF8() = responseBody.orEmpty()
+ */
 public class CustomFeignException extends FeignException {
     protected CustomFeignException(int status, String message, Request request, Throwable cause, byte[] responseBody) {
         super(status, message, request, cause, responseBody);
