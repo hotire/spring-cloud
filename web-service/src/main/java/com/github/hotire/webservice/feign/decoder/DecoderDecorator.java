42efc2e544817c07b103cd4e483878a5145b3543
hotire <gngh0101@gmail.com>
2021-10-20 00:11:44 +0900

    Add DecoderDecorator

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/decoder/DecoderDecorator.java
@@ -0,0 +1,21 @@
+package com.github.hotire.webservice.feign.decoder;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import feign.FeignException;
+import feign.Response;
+import feign.codec.DecodeException;
+import feign.codec.Decoder;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DecoderDecorator implements Decoder {
+
+    private final Decoder decoder;
+
+    @Override
+    public Object decode(Response response, Type type) throws IOException, DecodeException, FeignException {
+        return decoder.decode(response, type);
+    }
+}
