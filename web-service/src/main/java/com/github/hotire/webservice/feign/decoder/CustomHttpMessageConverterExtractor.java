3f85793c0fc57e6017898ac6402a87ad26a6f36d
hotire <gngh0101@gmail.com>
2021-10-20 23:55:17 +0900

    Polish

--- web-service/src/main/java/com/github/hotire/webservice/feign/decoder/CustomHttpMessageConverterExtractor.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/decoder/CustomHttpMessageConverterExtractor.java
@@ -5,25 +5,29 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.HttpMessageConverterExtractor;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CustomHttpMessageConverterExtractor<T> extends HttpMessageConverterExtractor<T> {
-    public CustomHttpMessageConverterExtractor(Class responseType, List list) {
-        super(responseType, list);
+
+    public CustomHttpMessageConverterExtractor(Class<T> responseType, List<HttpMessageConverter<?>> messageConverters) {
+        super(responseType, messageConverters);
     }
 
-    public CustomHttpMessageConverterExtractor(Type responseType, List list) {
-        super(responseType, list);
+    public CustomHttpMessageConverterExtractor(Type responseType, List<HttpMessageConverter<?>> messageConverters) {
+        super(responseType, messageConverters);
     }
 
     @Override
     protected MediaType getContentType(ClientHttpResponse response) {
         MediaType contentType = response.getHeaders().getContentType();
         if (contentType == null) {
-            if (log.isTraceEnabled()) log.trace("No content-type, using 'application/json'");
+            if (log.isTraceEnabled()) {
+                log.trace("No content-type, using 'application/json'");
+            }
             contentType = MediaType.APPLICATION_JSON;
         }
         return contentType;
