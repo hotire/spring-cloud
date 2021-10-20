51437292ae8ca48cf18a3dc1e74e9a533ed8238e
hotire <gngh0101@gmail.com>
2021-10-21 00:08:32 +0900

    Learn SpringDecoder

--- web-service/src/main/java/com/github/hotire/webservice/feign/decoder/SpringDecoderDecorator.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/decoder/SpringDecoderDecorator.java
@@ -3,6 +3,7 @@ package com.github.hotire.webservice.feign.decoder;
 import java.io.IOException;
 import java.lang.reflect.Type;
 
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 
 import feign.FeignException;
@@ -11,6 +12,12 @@ import feign.codec.DecodeException;
 import feign.codec.Decoder;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * SpringDecoder use HttpMessageConverters
+ *
+ * @see SpringDecoder#decode(Response, Type)
+ * @see HttpMessageConverters
+ */
 @RequiredArgsConstructor
 public class SpringDecoderDecorator implements Decoder {
     private final SpringDecoder decoder;
