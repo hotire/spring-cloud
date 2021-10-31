76261f66e4a5f84e891ad2694aa5862bbfd0fc7d
hotire <gngh0101@gmail.com>
2021-10-31 15:37:21 +0900

    Override isCandidateComponent of ClassPathScanningCandidateComponentProvider

--- web-service/src/main/java/com/github/hotire/webservice/feign/config/CustomClassPathScanningCandidateComponentProvider.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/config/CustomClassPathScanningCandidateComponentProvider.java
@@ -1,6 +1,39 @@
 package com.github.hotire.webservice.feign.config;
 
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 
 public class CustomClassPathScanningCandidateComponentProvider extends ClassPathScanningCandidateComponentProvider {
+    /**
+     * Feign
+     */
+    @Override
+    protected boolean isCandidateComponent(
+            AnnotatedBeanDefinition beanDefinition) {
+        boolean isCandidate = false;
+        if (beanDefinition.getMetadata().isIndependent()) {
+            if (!beanDefinition.getMetadata().isAnnotation()) {
+                isCandidate = true;
+            }
+        }
+        return isCandidate;
+    }
+
+    /**
+     * origin
+     */
+//    @Override
+//    public boolean isCandidateComponent(MetadataReader metadataReader) throws IOException {
+//        for (TypeFilter tf : this.excludeFilters) {
+//            if (tf.match(metadataReader, getMetadataReaderFactory())) {
+//                return false;
+//            }
+//        }
+//        for (TypeFilter tf : this.includeFilters) {
+//            if (tf.match(metadataReader, getMetadataReaderFactory())) {
+//                return isConditionMatch(metadataReader);
+//            }
+//        }
+//        return false;
+//    }
 }
