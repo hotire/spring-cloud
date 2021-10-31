72bbdb20b3f8bff17ab6f4e51b68023913f1ecc5
hotire <gngh0101@gmail.com>
2021-10-31 15:31:43 +0900

    Add FeignClientConfig

--- web-service/src/main/java/com/github/hotire/webservice/feign/config/FeignClientConfig.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/config/FeignClientConfig.java
@@ -1,8 +1,13 @@
-package com.github.hotire.webservice.feign;
+package com.github.hotire.webservice.feign.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * import
+ * @see org.springframework.cloud.openfeign.FeignClientsRegistrar
+ * a implemented ImportBeanDefinitionRegistrar
+ */
 @EnableFeignClients
 @Configuration
 public class FeignClientConfig {
