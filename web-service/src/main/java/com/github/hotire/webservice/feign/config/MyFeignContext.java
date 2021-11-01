7a44997dd08ffa98ad0fb78231e48578f82466e1
hotire <gngh0101@gmail.com>
2021-11-02 01:35:40 +0900

    Learn MyFeignContext

--- web-service/src/main/java/com/github/hotire/webservice/feign/config/MyFeignContext.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/config/MyFeignContext.java
@@ -1,6 +1,11 @@
 package com.github.hotire.webservice.feign.config;
 
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.cloud.openfeign.FeignContext;
 
+/**
+ * @see org.springframework.cloud.context.named.NamedContextFactory
+ * @see FeignAutoConfiguration#feignContext()
+ */
 public class MyFeignContext extends FeignContext {
 }
