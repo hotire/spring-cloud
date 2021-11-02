b91ad5b7494d71cf9c00f45db385bd3f434f80d3
hotire <gngh0101@gmail.com>
2021-11-03 00:20:14 +0900

    Add CustomRetryer

--- web-service/src/main/java/com/github/hotire/webservice/feign/retryer/CustomRetryer.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/retryer/CustomRetryer.java
@@ -2,11 +2,20 @@ package com.github.hotire.webservice.feign.retryer;
 
 import feign.RetryableException;
 import feign.Retryer;
+import lombok.RequiredArgsConstructor;
 
 /**
  * @see Retryer.Default
  */
+@RequiredArgsConstructor
 public class CustomRetryer implements Retryer {
+
+    private final int maxAttempts;
+    private final long period;
+    private final long maxPeriod;
+    int attempt;
+    long sleptForMillis;
+
     @Override
     public void continueOrPropagate(RetryableException e) {
 
@@ -14,6 +23,6 @@ public class CustomRetryer implements Retryer {
 
     @Override
     public Retryer clone() {
-        return this;
+        return new CustomRetryer(maxAttempts, period, maxPeriod);
     }
 }
