43d38a4ebc1e50be280685754133467e716f73ff
hotire <gngh0101@gmail.com>
2022-03-27 15:47:43 +0900

    Add FeignClientFactoryBeanCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/bean/FeignClientFactoryBeanCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.webservice.feign.bean;
+
+import org.springframework.cloud.openfeign.FeignClientFactoryBean;
+
+/**
+ * @see FeignClientFactoryBean
+ */
+public class FeignClientFactoryBeanCore {
+}
