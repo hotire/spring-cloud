3edf3c400d0a315dbd37205f06e51d0c8154306a
hotire <gngh0101@gmail.com>
2022-03-30 01:08:13 +0900

    Add FallbackConfig

--- web-service/src/main/java/com/github/hotire/webservice/feign/fallback/FallbackConfig.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/fallback/FallbackConfig.java
@@ -1,8 +1,16 @@
 package com.github.hotire.webservice.feign.fallback;
 
+import static feign.FeignException.errorStatus;
+
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.netflix.hystrix.HystrixCircuitBreakerFactory;
 import org.springframework.context.annotation.Bean;
+
+import feign.FeignException;
+import feign.Response;
+import feign.RetryableException;
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
 
 //@Configuration
 public class FallbackConfig {
@@ -17,5 +25,30 @@ public class FallbackConfig {
         };
     }
 
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new ErrorDecoder() {
+            @Override
+            public Exception decode(String methodKey, Response response) {
+                FeignException exception = errorStatus(methodKey, response);
+                int status = response.status();
+                if (status >= 400) {
+                    return new RetryableException(
+                            response.status(),
+                            exception.getMessage(),
+                            response.request().httpMethod(),
+                            exception,
+                            null,
+                            response.request());
+                }
+                return exception;
+            }
+        };
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, 100, 3);
+    }
 
 }
