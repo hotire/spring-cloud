360b6161160ba5871953ef3e778d64d2f0aa74f4
hotire <gngh0101@gmail.com>
2022-04-03 16:35:16 +0900

    Apply IgnoreFallbackFeignException

--- web-service/src/main/java/com/github/hotire/webservice/feign/fallback/FallbackConfig.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/fallback/FallbackConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 
 import feign.FeignException;
 import feign.Response;
-import feign.RetryableException;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
 
@@ -33,13 +32,15 @@ public class FallbackConfig {
                 FeignException exception = errorStatus(methodKey, response);
                 int status = response.status();
                 if (status >= 400) {
-                    return new RetryableException(
-                            response.status(),
-                            exception.getMessage(),
-                            response.request().httpMethod(),
-                            exception,
-                            null,
-                            response.request());
+//                    return new RetryableException(
+//                            response.status(),
+//                            exception.getMessage(),
+//                            response.request().httpMethod(),
+//                            exception,
+//                            null,
+//                            response.request());
+//                    return new IgnoreFallbackFeignException(1, "");
+                    return new IgnoreFallbackFeignException(exception);
                 }
                 return exception;
             }
