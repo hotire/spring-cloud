a380fcdf2a767904b165205544914a14e6e5832b
hotire <gngh0101@gmail.com>
2022-04-06 02:10:26 +0900

    Add HystrixConfig

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/fallback/hystrix/HystrixCircuitBreakerFactoryCustom.java
@@ -0,0 +1,13 @@
+package com.github.hotire.webservice.feign.fallback.hystrix;
+
+public class HystrixCircuitBreakerFactoryCustom {
+    //    @Bean
+//    public Customizer<HystrixCircuitBreakerFactory> customizer() {
+//        return new Customizer<HystrixCircuitBreakerFactory>() {
+//            @Override
+//            public void customize(HystrixCircuitBreakerFactory factory) {
+////                factory.configure();
+//            }
+//        };
+//    }
+}
---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/fallback/hystrix/HystrixConfig.java
@@ -0,0 +1,16 @@
+package com.github.hotire.webservice.feign.fallback.hystrix;
+
+/**
+// * @see org.springframework.cloud.openfeign.FeignClientsRegistrar
+ */
+public class HystrixConfig {
+//    @PostConstruct
+//    public void init() {
+//        HystrixPlugins.getInstance().registerEventNotifier(new HystrixEventNotifier() {
+//            @Override
+//            public void markEvent(HystrixEventType eventType, HystrixCommandKey key) {
+//                log.info("eventType : {}", eventType);
+//            }
+//        });
+//    }
+}
