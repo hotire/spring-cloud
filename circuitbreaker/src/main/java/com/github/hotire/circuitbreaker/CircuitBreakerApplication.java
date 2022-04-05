128411e7bbe769bf159442c46b9874b326b73295
hotire <gngh0101@gmail.com>
2022-04-05 23:17:47 +0900

    init circuitbreaker module

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/CircuitBreakerApplication.java
@@ -0,0 +1,11 @@
+package com.github.hotire.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerApplication {
+    public static void main(String... args) {
+        SpringApplication.run(CircuitBreakerApplication.class);
+    }
+}
