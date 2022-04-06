f6abef369dc7bfe4ac4372a20093e26ac1e0e5e1
hotire <gngh0101@gmail.com>
2022-04-06 23:38:32 +0900

    Add CircuitBreakerFactoryCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/fallback/CircuitBreakerFactoryCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.webservice.feign.fallback;
+
+/**
+ * @see org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory
+ */
+public class CircuitBreakerFactoryCore {
+}
