a0068f1a500a59bfff1267e3efb0f6cc1281c8fb
hotire <gngh0101@gmail.com>
2022-04-08 01:03:06 +0900

    Add HystrixCircuitBreakerFactoryCore

--- circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/hystrix/HystrixCircuitBreakerFactoryCore.java
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/hystrix/HystrixCircuitBreakerFactoryCore.java
@@ -4,5 +4,5 @@ package com.github.hotire.circuitbreaker.hystrix;
  * @see org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory
  * @see org.springframework.cloud.netflix.hystrix.HystrixCircuitBreakerFactory
  */
-public class CircuitBreakerFactoryCore {
+public class HystrixCircuitBreakerFactoryCore {
 }
