95800ac6582c609d24783b0ed50067eb21deeb0c
hotire <gngh0101@gmail.com>
2022-04-08 01:06:45 +0900

    Add ReflectiveFeignCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/feign_target/ReflectiveFeignCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.webservice.feign.feign_target;
+
+/**
+ * @see feign.ReflectiveFeign
+ * @see feign.ReflectiveFeign.FeignInvocationHandler
+ */
+public class ReflectiveFeignCore {
+}
