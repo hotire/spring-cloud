0de74b0531dc532b23ff77232804001b7484a2a3
hotire <gngh0101@gmail.com>
2022-04-09 01:25:53 +0900

    Learn InvocationHandlerFactoryCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/feign_target/InvocationHandlerFactoryCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.webservice.feign.feign_target;
+
+import feign.InvocationHandlerFactory;
+
+/**
+ * @see InvocationHandlerFactory
+ * @see feign.SynchronousMethodHandler.Factory
+ */
+public class InvocationHandlerFactoryCore {
+}
