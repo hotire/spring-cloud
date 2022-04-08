38dda06cbfcfc145d4c095b7307545645f86d723
hotire <gngh0101@gmail.com>
2022-04-09 01:25:34 +0900

    Learn MethodHandlerCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/feign_target/MethodHandlerCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.webservice.feign.feign_target;
+
+/**
+ * @see feign.InvocationHandlerFactory.MethodHandler
+ * @see feign.SynchronousMethodHandler
+ */
+public class MethodHandlerCore {
+}
