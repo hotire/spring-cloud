e167668907835f06274b74137b9746d585c800cf
hotire <gngh0101@gmail.com>
2022-04-10 17:01:05 +0900

    Add CircuitBreakerConfigCore

--- circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/circuitbreaker/CircuitBreakerConfigCore.java
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/circuitbreaker/CircuitBreakerConfigCore.java
@@ -1,4 +1,7 @@
-package com.github.hotire.circuitbreaker.resilience4j;
+package com.github.hotire.circuitbreaker.resilience4j.circuitbreaker;
 
+/**
+ * @see io.github.resilience4j.circuitbreaker.configure.CircuitBreakerConfiguration
+ */
 public class CircuitBreakerConfigCore {
 }
