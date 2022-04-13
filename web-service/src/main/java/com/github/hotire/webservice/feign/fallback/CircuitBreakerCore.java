5b2ea0b1ba12949c4f42e8fac52a3001f472b028
hotire <gngh0101@gmail.com>
2022-04-14 00:30:46 +0900

    Add CircuitBreakerCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/fallback/CircuitBreakerCore.java
@@ -0,0 +1,20 @@
+package com.github.hotire.webservice.feign.fallback;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
+
+/**
+ * @see org.springframework.cloud.client.circuitbreaker.CircuitBreaker
+ */
+public interface CircuitBreakerCore {
+
+    default <T> T run(Supplier<T> toRun) {
+        return run(toRun, throwable -> {
+            throw new NoFallbackAvailableException("No fallback available.", throwable);
+        });
+    };
+
+    <T> T run(Supplier<T> toRun, Function<Throwable, T> fallback);
+}
