4b663db5cae391ce72c8a244c66a5f1aab62da77
hotire <gngh0101@gmail.com>
2022-04-18 00:48:46 +0900

    Add FeignBuilderCore

--- web-service/src/main/java/com/github/hotire/webservice/feign/feign_target/FeignBuilderCore.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/feign_target/FeignBuilderCore.java
@@ -2,6 +2,7 @@ package com.github.hotire.webservice.feign.feign_target;
 
 /**
  * @see feign.Feign.Builder
+ * @see org.springframework.cloud.openfeign.FeignCircuitBreaker.Builder
  */
 public class FeignBuilderCore {
 }
