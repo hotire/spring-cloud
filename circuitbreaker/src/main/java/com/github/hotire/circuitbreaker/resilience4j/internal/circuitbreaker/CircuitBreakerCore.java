44774700f0a1175bcf40cbf76b8717b65fa224a0
hotire <gngh0101@gmail.com>
2022-04-28 00:12:18 +0900

    Learn CircuitBreakerCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/internal/circuitbreaker/CircuitBreakerCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.circuitbreaker.resilience4j.internal.circuitbreaker;
+
+/**
+ * @see io.github.resilience4j.circuitbreaker.CircuitBreaker
+ * @see io.github.resilience4j.circuitbreaker.internal.CircuitBreakerStateMachine
+ */
+public class CircuitBreakerCore {
+}
