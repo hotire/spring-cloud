c22ea11442855ec067ccbb6cd5826f14fbf710f0
hotire <gngh0101@gmail.com>
2022-04-29 00:46:07 +0900

    Learn ClientCore

--- web-service/src/main/java/com/github/hotire/webservice/feign/client/ClientCore.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/client/ClientCore.java
@@ -1,6 +1,7 @@
 package com.github.hotire.webservice.feign.client;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import feign.Client;
 import feign.Request;
@@ -12,8 +13,18 @@ import feign.Response;
  * @see Client.Default
  */
 public class ClientCore implements Client {
+    /**
+     * @see Client.Default#execute(Request, Options)
+     */
     @Override
     public Response execute(Request request, Options options) throws IOException {
+        return null;
+    }
+
+    /**
+     * @see Client.Default#convertAndSend(Request, Options)
+     */
+    HttpURLConnection convertAndSend(Request request, Options options) throws IOException {
         return null;
     }
 }
