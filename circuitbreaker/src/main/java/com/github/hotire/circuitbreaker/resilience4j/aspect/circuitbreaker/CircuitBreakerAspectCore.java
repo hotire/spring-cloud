33d2557a471eac522fcf22bff820b7db47708714
hotire <gngh0101@gmail.com>
2022-04-29 00:20:07 +0900

    Add CircuitBreakerAspect

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/aspect/circuitbreaker/CircuitBreakerAspectCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.circuitbreaker.resilience4j.aspect.circuitbreaker;
+
+/**
+ * @see io.github.resilience4j.circuitbreaker.configure.CircuitBreakerAspect
+ */
+public class CircuitBreakerAspectCore {
+}
