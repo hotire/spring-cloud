42d3ce9aa0d4bbd29a7c3eae965777e333780a13
hotire <gngh0101@gmail.com>
2022-05-01 16:31:17 +0900

    Add Resilience4JCircuitBreakerCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/cloud/circuitbreaker/Resilience4JCircuitBreakerCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.circuitbreaker.resilience4j.cloud.circuitbreaker;
+
+/**
+ * @see org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker
+ */
+public class Resilience4JCircuitBreakerCore {
+}
