6535703f193ae45106a4ab04a1a982e544a4e56a
hotire <gngh0101@gmail.com>
2022-07-28 10:07:09 +0900

    Add TimeLimiterCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/aspect/timelimiter/TimeLimiterCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.circuitbreaker.resilience4j.aspect.timelimiter;
+
+/**
+ * @see io.github.resilience4j.timelimiter.annotation.TimeLimiter
+ */
+public class TimeLimiterCore {
+}
