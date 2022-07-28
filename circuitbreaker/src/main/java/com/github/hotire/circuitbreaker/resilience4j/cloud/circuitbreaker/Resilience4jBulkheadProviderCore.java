03632a1055c05ba3a24f5943d71a29b0a0e4cf96
hotire <gngh0101@gmail.com>
2022-07-28 10:07:21 +0900

    Add Resilience4jBulkheadProviderCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/cloud/circuitbreaker/Resilience4jBulkheadProviderCore.java
@@ -0,0 +1,24 @@
+package com.github.hotire.circuitbreaker.resilience4j.cloud.circuitbreaker;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4jBulkheadProvider;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.timelimiter.TimeLimiter;
+import io.vavr.collection.Map;
+
+/**
+ * @see Resilience4jBulkheadProvider
+ */
+public class Resilience4jBulkheadProviderCore {
+
+    /**
+     * @see Resilience4jBulkheadProvider#run(String, Supplier, Function, CircuitBreaker, TimeLimiter, Map)
+     */
+    public <T> T run(String id, Supplier<T> toRun, Function<Throwable, T> fallback, CircuitBreaker circuitBreaker,
+                     TimeLimiter timeLimiter, io.vavr.collection.Map<String, String> tags) {
+        return null;
+    }
+}
