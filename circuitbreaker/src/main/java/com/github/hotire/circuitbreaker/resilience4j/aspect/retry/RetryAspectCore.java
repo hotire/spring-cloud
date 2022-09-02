26c60000af694630e47b09f2dbfaee193cc12f09
hotire <gngh0101@gmail.com>
2022-09-02 17:52:30 +0900

    Learn ReyryCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/aspect/retry/RetryAspectCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.circuitbreaker.resilience4j.aspect.retry;
+
+import io.github.resilience4j.retry.configure.RetryAspect;
+
+/**
+ * @see RetryAspect
+ */
+public class RetryAspectCore {
+
+}
---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/aspect/retry/RetryCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.circuitbreaker.resilience4j.aspect.retry;
+
+/**
+ * @see io.github.resilience4j.retry.annotation.Retry;
+ */
+public class RetryCore {
+
+}
