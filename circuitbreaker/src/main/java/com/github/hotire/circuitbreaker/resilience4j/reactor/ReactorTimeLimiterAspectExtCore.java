0768d3cdd4a4f10c21dd9207fe6ee83a065b4cdf
hotire <gngh0101@gmail.com>
2022-09-05 07:57:51 +0900

    Update TimeLimiterAspectExt

--- circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/reactor/ReactorTimeLimiterAspectExtCore.java
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/reactor/ReactorTimeLimiterAspectExtCore.java
@@ -1,6 +1,7 @@
 package com.github.hotire.circuitbreaker.resilience4j.reactor;
 
 /**
+ * @see io.github.resilience4j.timelimiter.configure.TimeLimiterAspectExt
  * @see io.github.resilience4j.timelimiter.configure.ReactorTimeLimiterAspectExt
  */
 public class ReactorTimeLimiterAspectExtCore {
