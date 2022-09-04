21e11251a1bcbb86b750e90b6d92e11a7c230181
hotire <gngh0101@gmail.com>
2022-09-04 22:38:31 +0900

    Learn reactor timeLimiter

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/reactor/ReactorTimeLimiterAspectExtCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.circuitbreaker.resilience4j.reactor;
+
+/**
+ * @see io.github.resilience4j.timelimiter.configure.ReactorTimeLimiterAspectExt
+ */
+public class ReactorTimeLimiterAspectExtCore {
+
+}
