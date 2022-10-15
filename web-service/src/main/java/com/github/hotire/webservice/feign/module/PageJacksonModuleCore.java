4bbd4650e405855778f79552aa280c324cd661ca
hotire <gngh0101@gmail.com>
2022-10-15 15:32:49 +0900

    Update PageJacksonModuleCore

--- web-service/src/main/java/com/github/hotire/webservice/feign/module/PageJacksonModuleCore.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/module/PageJacksonModuleCore.java
@@ -1,7 +1,10 @@
 package com.github.hotire.webservice.feign.module;
 
+
 /**
  * @see org.springframework.cloud.openfeign.support.PageJacksonModule
+ * feign.autoconfiguration.jackson.enabled
+ * @see org.springframework.cloud.openfeign.FeignAutoConfiguration.FeignJacksonConfiguration
  */
 public class PageJacksonModuleCore {
 
