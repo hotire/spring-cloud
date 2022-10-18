b37d0b394cdafff3760a93552e650cdbbbbecc13
hotire <gngh0101@gmail.com>
2022-10-19 01:22:50 +0900

    Add Sleuth module

---
+++ slueth/src/main/java/com/github/hotire/SleuthApplication.java
@@ -0,0 +1,12 @@
+package com.github.hotire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(SleuthApplication.class);
+    }
+}
