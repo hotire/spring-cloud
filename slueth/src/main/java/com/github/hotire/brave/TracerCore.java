043a080b3ca5bb44fb3e4ced81bd9b31ea3c1ca5
hotire <gngh0101@gmail.com>
2022-10-20 01:55:32 +0900

    Learn brave

---
+++ slueth/src/main/java/com/github/hotire/brave/TracerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.brave;
+
+import brave.Tracer;
+
+/**
+ * @see Tracer
+ */
+public class TracerCore {
+
+}
---
+++ slueth/src/main/java/com/github/hotire/brave/TracingCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.brave;
+
+import brave.Tracing;
+
+/**
+ * @see Tracing
+ * @see brave.Tracing.Default;
+ */
+public class TracingCore {
+
+}
