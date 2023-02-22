fd3baa37d5fa72312619671ee4fcf3957cb10c0c
hotire <gngh0101@gmail.com>
2023-02-22 23:09:37 +0900

    RateLimiterCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/rate_limiter/RateLimiterCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.circuitbreaker.resilience4j.core.rate_limiter;
+
+import io.github.resilience4j.ratelimiter.RateLimiter;
+
+/**
+ * @see RateLimiter
+ */
+public class RateLimiterCore {
+
+}
