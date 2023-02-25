a3a48d0a4fb5b30cc7e07440bb7758d52f0d341f
hotire <gngh0101@gmail.com>
2023-02-25 15:54:16 +0900

    Add AtomicRateLimiter

--- circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/rate_limiter/RateLimiterCore.java
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/rate_limiter/RateLimiterCore.java
@@ -1,9 +1,11 @@
 package com.github.hotire.circuitbreaker.resilience4j.core.rate_limiter;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.internal.AtomicRateLimiter;
 
 /**
  * @see RateLimiter
+ * @see AtomicRateLimiter
  */
 public class RateLimiterCore {
 
