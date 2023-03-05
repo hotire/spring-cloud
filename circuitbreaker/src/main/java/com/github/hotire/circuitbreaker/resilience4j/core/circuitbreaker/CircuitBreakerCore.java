a6ae8c8476c1d5db825816eba9755cddb2de5a39
hotire <gngh0101@gmail.com>
2023-03-05 13:59:17 +0900

    Add CircuitBreakerCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/circuitbreaker/CircuitBreakerCore.java
@@ -0,0 +1,19 @@
+package com.github.hotire.circuitbreaker.resilience4j.core.circuitbreaker;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.internal.CircuitBreakerStateMachine;
+
+/**
+ * @see CircuitBreaker
+ * @see CircuitBreakerStateMachine
+ */
+public class CircuitBreakerCore {
+
+    /**
+     * @see CircuitBreaker#acquirePermission()
+     * @see CircuitBreakerStateMachine#acquirePermission()
+     */
+    void acquirePermission() {
+
+    }
+}
