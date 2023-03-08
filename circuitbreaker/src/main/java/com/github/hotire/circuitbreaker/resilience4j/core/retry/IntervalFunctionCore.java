cb55c1667a995f3c62c7aea61d3338cf20f13755
hotire <gngh0101@gmail.com>
2023-03-09 03:00:13 +0900

    Add DEFAULT_INITIAL_INTERVAL, DEFAULT_MULTIPLIER, DEFAULT_RANDOMIZATION_FACTOR

--- circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/retry/IntervalFunctionCore.java
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/retry/IntervalFunctionCore.java
@@ -7,4 +7,16 @@ import io.github.resilience4j.core.IntervalFunction;
  */
 public class IntervalFunctionCore {
 
+    /**
+     * @see IntervalFunction#DEFAULT_INITIAL_INTERVAL
+     */
+    long DEFAULT_INITIAL_INTERVAL = 500;
+    /**
+     * @see IntervalFunction#DEFAULT_MULTIPLIER
+     */
+    double DEFAULT_MULTIPLIER = 1.5;
+    /**
+     * @see IntervalFunction#DEFAULT_RANDOMIZATION_FACTOR
+     */
+    double DEFAULT_RANDOMIZATION_FACTOR = 0.5;
 }
