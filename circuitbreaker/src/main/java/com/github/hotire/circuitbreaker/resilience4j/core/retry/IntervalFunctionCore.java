6d9dcf4896f06165e9ad695a5642ea699cc4f1a0
hotire <gngh0101@gmail.com>
2023-03-09 02:59:24 +0900

    Add IntervalFunctionCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/retry/IntervalFunctionCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.circuitbreaker.resilience4j.core.retry;
+
+import io.github.resilience4j.core.IntervalFunction;
+
+/**
+ * @see IntervalFunction
+ */
+public class IntervalFunctionCore {
+
+}
