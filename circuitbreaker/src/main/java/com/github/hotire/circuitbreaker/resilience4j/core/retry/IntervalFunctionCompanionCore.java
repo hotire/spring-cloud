7604d5dcb771fd5f2ebea911923bf06c56ccefb4
hotire <gngh0101@gmail.com>
2023-03-11 21:16:24 +0900

    Add IntervalFunctionCompanionCore

---
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/retry/IntervalFunctionCompanionCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.circuitbreaker.resilience4j.core.retry;
+
+/**
+ * @see io.github.resilience4j.core.IntervalFunctionCompanion
+ */
+public class IntervalFunctionCompanionCore {
+
+}
