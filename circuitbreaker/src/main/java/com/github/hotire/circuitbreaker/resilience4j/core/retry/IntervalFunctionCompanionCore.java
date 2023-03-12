b3520f1bcc2e6b25057bc518ef1c60717eff1512
hotire <gngh0101@gmail.com>
2023-03-12 23:15:23 +0900

    randomize

--- circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/retry/IntervalFunctionCompanionCore.java
+++ circuitbreaker/src/main/java/com/github/hotire/circuitbreaker/resilience4j/core/retry/IntervalFunctionCompanionCore.java
@@ -5,4 +5,14 @@ package com.github.hotire.circuitbreaker.resilience4j.core.retry;
  */
 public class IntervalFunctionCompanionCore {
 
+    /**
+     * @see io.github.resilience4j.core.IntervalFunctionCompanion#randomize(double, double) 
+     */
+    static double randomize(final double current, final double randomizationFactor) {
+        final double delta = randomizationFactor * current;
+        final double min = current - delta;
+        final double max = current + delta;
+
+        return (min + (Math.random() * (max - min + 1)));
+    }
 }
