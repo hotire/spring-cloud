6683e0037999f6bfab47e420cfd289f0815bef90
hotire <gngh0101@gmail.com>
2023-03-15 02:34:26 +0900

    Override clone

--- web-service/src/main/java/com/github/hotire/webservice/feign/retryer/JitterBasedRetryer.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/retryer/JitterBasedRetryer.java
@@ -1,13 +1,24 @@
 package com.github.hotire.webservice.feign.retryer;
 
+import feign.RetryableException;
+import feign.Retryer;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class JitterBasedRetryer {
+public class JitterBasedRetryer implements Retryer  {
     private final int maxAttempts;
     private final long period;
     private final long maxPeriod;
     int attempt;
     long sleptForMillis;
 
+    @Override
+    public void continueOrPropagate(RetryableException e) {
+
+    }
+
+    @Override
+    public Retryer clone() {
+        return new JitterBasedRetryer(maxAttempts, period, maxPeriod);
+    }
 }
