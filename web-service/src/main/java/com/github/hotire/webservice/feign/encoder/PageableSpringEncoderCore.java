f41d8e1d4dcec5e2cc855679c36e806d95af3fbc
hotire <gngh0101@gmail.com>
2023-04-05 02:23:27 +0900

    Add PageableSpringEncoderCore

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/encoder/PageableSpringEncoderCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.webservice.feign.encoder;
+
+/**
+ * @see org.springframework.cloud.openfeign.support.PageableSpringEncoder
+ */
+public class PageableSpringEncoderCore {
+
+}
