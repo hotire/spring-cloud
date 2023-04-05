fc28150b9e6fefd66dd78097db7b909a015c8919
hotire <gngh0101@gmail.com>
2023-04-06 02:28:42 +0900

    encode

--- web-service/src/main/java/com/github/hotire/webservice/feign/encoder/PageableSpringEncoderCore.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/encoder/PageableSpringEncoderCore.java
@@ -1,8 +1,19 @@
 package com.github.hotire.webservice.feign.encoder;
 
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import java.lang.reflect.Type;
+import org.springframework.cloud.openfeign.support.PageableSpringEncoder;
+
 /**
- * @see org.springframework.cloud.openfeign.support.PageableSpringEncoder
+ * @see PageableSpringEncoder;
  */
 public class PageableSpringEncoderCore {
 
+    /**
+     * @see PageableSpringEncoder#encode(Object, Type, RequestTemplate) 
+     */
+    public void encode(Object object, Type bodyType, RequestTemplate template) throws EncodeException {
+
+    }
 }
