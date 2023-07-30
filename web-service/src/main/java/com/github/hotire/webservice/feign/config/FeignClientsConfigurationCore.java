6701778cd1917d292d06efee76be1bb2a6e22305
hotire <gngh0101@gmail.com>
2023-07-31 00:16:41 +0900

    Add feignDecoder

--- web-service/src/main/java/com/github/hotire/webservice/feign/config/FeignClientsConfigurationCore.java
+++ web-service/src/main/java/com/github/hotire/webservice/feign/config/FeignClientsConfigurationCore.java
@@ -1,10 +1,20 @@
 package com.github.hotire.webservice.feign.config;
 
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.cloud.openfeign.support.HttpMessageConverterCustomizer;
 
 /**
  * @see FeignClientsConfiguration
  */
 public class FeignClientsConfigurationCore {
 
+
+    /**
+     * @see FeignClientsConfiguration#feignDecoder(ObjectProvider) 
+     */
+    public Decoder feignDecoder(ObjectProvider<HttpMessageConverterCustomizer> customizers) {
+        return null;
+    }
 }
