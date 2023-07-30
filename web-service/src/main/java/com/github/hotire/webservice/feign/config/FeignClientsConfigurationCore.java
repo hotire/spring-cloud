fbc5ec718c588d3f937c0a91fcbebc84e412ce3e
hotire <gngh0101@gmail.com>
2023-07-31 00:15:01 +0900

    Add FeignClientsConfiguration

---
+++ web-service/src/main/java/com/github/hotire/webservice/feign/config/FeignClientsConfigurationCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.webservice.feign.config;
+
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+
+/**
+ * @see FeignClientsConfiguration
+ */
+public class FeignClientsConfigurationCore {
+
+}
