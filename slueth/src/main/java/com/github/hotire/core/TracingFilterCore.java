2e0f0d7777d08106ae11225ca0d591fe2c53613d
hotire <gngh0101@gmail.com>
2024-07-04 02:39:07 +0900

    Add TracingFilterCore

---
+++ slueth/src/main/java/com/github/hotire/core/TracingFilterCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.core;
+
+import org.springframework.cloud.sleuth.instrument.web.servlet.TracingFilter;
+
+/**
+ * @see TracingFilter
+ */
+public class TracingFilterCore {
+
+}
