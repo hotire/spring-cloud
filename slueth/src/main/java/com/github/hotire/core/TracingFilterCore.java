9924532dfbbc8bffc63303724b817d27694b374c
hotire <gngh0101@gmail.com>
2024-07-05 01:42:11 +0900

    Update TracingFilterCore

--- slueth/src/main/java/com/github/hotire/core/TracingFilterCore.java
+++ slueth/src/main/java/com/github/hotire/core/TracingFilterCore.java
@@ -1,10 +1,17 @@
 package com.github.hotire.core;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.sleuth.CurrentTraceContext;
+import org.springframework.cloud.sleuth.http.HttpServerHandler;
 import org.springframework.cloud.sleuth.instrument.web.servlet.TracingFilter;
 
 /**
  * @see TracingFilter
  */
+@RequiredArgsConstructor
 public class TracingFilterCore {
 
+    final CurrentTraceContext currentTraceContext;
+
+    final HttpServerHandler handler;
 }
