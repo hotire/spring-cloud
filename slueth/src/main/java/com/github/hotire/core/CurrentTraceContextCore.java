31691a1379d006fa52c844770408a2b6b08307fa
hotire <gngh0101@gmail.com>
2024-07-05 01:41:44 +0900

    Add CurrentTraceContextCore

---
+++ slueth/src/main/java/com/github/hotire/core/CurrentTraceContextCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.core;
+
+import org.springframework.cloud.sleuth.CurrentTraceContext;
+
+/**
+ * @see CurrentTraceContext
+ */
+public class CurrentTraceContextCore {
+
+}
