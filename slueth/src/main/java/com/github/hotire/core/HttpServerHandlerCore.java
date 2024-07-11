dcd35e73068993970ef78ee6edcfecdb21b53c42
hotire <gngh0101@gmail.com>
2024-07-12 02:44:02 +0900

    Add HttpServerHandlerCore

--- slueth/src/main/java/com/github/hotire/core/HttpServerHandlerCore.java
+++ slueth/src/main/java/com/github/hotire/core/HttpServerHandlerCore.java
@@ -1,9 +1,12 @@
 package com.github.hotire.core;
 
 import org.springframework.cloud.sleuth.http.HttpServerHandler;
+import org.springframework.cloud.sleuth.brave.bridge.BraveHttpServerHandler;
+
 
 /**
  * @see HttpServerHandler
+ * @see BraveHttpServerHandler
  */
 public class HttpServerHandlerCore {
 
