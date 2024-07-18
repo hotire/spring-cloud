2a91c0b86d1b9f6991660b3fdb4fa68142304667
hotire <gngh0101@gmail.com>
2024-07-18 23:26:07 +0900

    Update CurrentTraceContextCore

--- slueth/src/main/java/com/github/hotire/core/CurrentTraceContextCore.java
+++ slueth/src/main/java/com/github/hotire/core/CurrentTraceContextCore.java
@@ -1,9 +1,13 @@
 package com.github.hotire.core;
 
 import org.springframework.cloud.sleuth.CurrentTraceContext;
+import org.springframework.cloud.sleuth.brave.bridge.BraveCurrentTraceContext;
 
 /**
  * @see CurrentTraceContext
+ * @see BraveCurrentTraceContext
+ * @see brave.propagation.CurrentTraceContext
+ * @see brave.propagation.ThreadLocalCurrentTraceContext
  */
 public class CurrentTraceContextCore {
 
