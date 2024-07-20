18eb691dad3b5e509e009b04b23152cf43cb9800
hotire <gngh0101@gmail.com>
2024-07-20 10:24:56 +0900

    Update decorateScope in CurrentTraceContextCore

--- slueth/src/main/java/com/github/hotire/core/CurrentTraceContextCore.java
+++ slueth/src/main/java/com/github/hotire/core/CurrentTraceContextCore.java
@@ -1,5 +1,8 @@
 package com.github.hotire.core;
 
+import brave.internal.Nullable;
+import brave.propagation.CurrentTraceContext.Scope;
+import brave.propagation.TraceContext;
 import org.springframework.cloud.sleuth.CurrentTraceContext;
 import org.springframework.cloud.sleuth.brave.bridge.BraveCurrentTraceContext;
 
@@ -11,4 +14,14 @@ import org.springframework.cloud.sleuth.brave.bridge.BraveCurrentTraceContext;
  */
 public class CurrentTraceContextCore {
 
+    /**
+     * @see brave.propagation.CurrentTraceContext#decorateScope(TraceContext, Scope)
+     * @see brave.propagation.ThreadLocalCurrentTraceContext#decorateScope(TraceContext, Scope)
+     * @param context
+     * @param scope
+     * @return
+     */
+    protected Scope decorateScope(@Nullable TraceContext context, Scope scope) {
+        return scope;
+    }
 }
