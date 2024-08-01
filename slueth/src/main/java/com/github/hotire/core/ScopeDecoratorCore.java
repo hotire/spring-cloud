d4e58db94eb3e101a10d69309757b409ffdf03fa
hotire <gngh0101@gmail.com>
2024-08-01 16:55:43 +0900

    Add CorrelationScopeDecorator

--- slueth/src/main/java/com/github/hotire/core/ScopeDecoratorCore.java
+++ slueth/src/main/java/com/github/hotire/core/ScopeDecoratorCore.java
@@ -2,6 +2,7 @@ package com.github.hotire.core;
 
 /**
  * @see brave.propagation.CurrentTraceContext.ScopeDecorator
+ * @see brave.baggage.CorrelationScopeDecorator
  */
 public class ScopeDecoratorCore {
 
